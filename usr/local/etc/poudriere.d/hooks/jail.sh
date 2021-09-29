9c6e0b78a4316eebeacc3a17e6cbf5d04753d439
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2021-09-29 18:00:15 +0000

    Add a Poudriere hook to mount host directories.
    The hook mounts in a jail the CHERI SDK and host libraries that are used
    by the host toolchain wrapper.

---
+++ usr/local/etc/poudriere.d/hooks/jail.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+event="${1}"
+
+case "${event}" in
+mount)
+	mntpath="${2}"
+
+	mkdir -p "${mntpath}/zdata/cheri/output"
+	mount -r -t nullfs /zdata/cheri/output "${mntpath}/zdata/cheri/output"
+
+	mkdir -p "${mntpath}/host/lib" "${mntpath}/host/usr/lib"
+	mount -r -t nullfs /lib "${mntpath}/host/lib"
+	mount -r -t nullfs /usr/lib "${mntpath}/host/usr/lib"
+	;;
+*)
+	;;
+esac
