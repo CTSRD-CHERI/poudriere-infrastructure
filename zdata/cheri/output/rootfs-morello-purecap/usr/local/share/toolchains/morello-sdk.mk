66ed81e1a3676679954ceb7086252d5d429a2468
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-02-07 11:04:33 +0000

    Use an external toolchain.
    Instead of setting environment variables for specific toolchain tools,
    use the external toolchain support in the ports build system.

---
+++ zdata/cheri/output/rootfs-morello-purecap/usr/local/share/toolchains/morello-sdk.mk
@@ -0,0 +1,5 @@
+XCC=			/zdata/cheri/output/jail-morello-purecap/clang
+XCPP=			/zdata/cheri/output/jail-morello-purecap/clang-cpp
+XCXX=			/zdata/cheri/output/jail-morello-purecap/clang++
+CROSS_BINUTILS_PREFIX=	/zdata/cheri/output/jail-morello-purecap/
+X_COMPILER_TYPE=	clang
