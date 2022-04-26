80bd31b0cc428a8a860d48b5795de6ccc4d6efad
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-04-26 19:40:13 +0100

    Add a hybrid ABI cross toolchain.
    The toolchain sets compiler paths to a hybrid ABI SDK and a cross host
    to aarch64-portbld-freebsd14.0 to make the cross host different from
    a target, i.e. aarch64-unknown-freebsd14.0.

---
+++ overlay/zdata/cheri/output/rootfs-aarch64/usr/local64/share/toolchains/aarch64-hybrid-sdk.mk
@@ -0,0 +1,10 @@
+CC=			/zdata/cheri/output/jail-morello-hybrid/clang
+XCC=			/zdata/cheri/output/jail-morello-hybrid/clang
+CPP=			/zdata/cheri/output/jail-morello-hybrid/clang-cpp
+XCPP=			/zdata/cheri/output/jail-morello-hybrid/clang-cpp
+CXX=			/zdata/cheri/output/jail-morello-hybrid/clang++
+XCXX=			/zdata/cheri/output/jail-morello-hybrid/clang++
+CROSS_BINUTILS_PREFIX=	/zdata/cheri/output/jail-morello-hybrid/
+X_COMPILER_TYPE=	clang
+
+CROSS_HOST=		aarch64-portbld-freebsd14.0
