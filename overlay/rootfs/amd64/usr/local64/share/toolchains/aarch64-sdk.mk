9244903749e60d83177170ea2d7b6cee43db4a7e
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-04-14 14:26:28 +0100

    Add overlay for an amd64 jail
    The amd64 jail can be used to build packages with the local base set to
    /usr/local64 that would match hybrid ABI packages for CheriBSD.
    Such packages can be used as replacement packages to build CheriABI
    packages using the QEMU user mode for CheriABI on an amd64 host.
    
    The toolchain name indicates that it's for aarch64 but it works for any
    native ABI build. This should be cleaned up in a separate commit.

---
+++ overlay/rootfs/amd64/usr/local64/share/toolchains/aarch64-sdk.mk
@@ -0,0 +1,8 @@
+CC=			/zdata/cheri/output/jail-aarch64/clang
+XCC=			/zdata/cheri/output/jail-aarch64/clang
+CPP=			/zdata/cheri/output/jail-aarch64/clang-cpp
+XCPP=			/zdata/cheri/output/jail-aarch64/clang-cpp
+CXX=			/zdata/cheri/output/jail-aarch64/clang++
+XCXX=			/zdata/cheri/output/jail-aarch64/clang++
+CROSS_BINUTILS_PREFIX=	/zdata/cheri/output/jail-aarch64/
+X_COMPILER_TYPE=	clang
