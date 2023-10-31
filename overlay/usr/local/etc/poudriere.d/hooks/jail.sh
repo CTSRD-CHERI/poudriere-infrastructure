146b9dde0a52389369f630dc0cbe149cdc568808
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-10-31 12:11:38 +0000

    Create a host hints file when starting a jail
    Not to modify the rc.d ldconfig script, run ldconfig in a jail hook for
    host binaries.

--- overlay/usr/local/etc/poudriere.d/hooks/jail.sh
+++ overlay/usr/local/etc/poudriere.d/hooks/jail.sh
@@ -6,10 +6,15 @@ case "${event}" in
 mount)
 	mntpath="${2}"
 
+	# Mount directories with host libraries.
 	mkdir -p "${mntpath}/host/lib" "${mntpath}/host/usr/lib" "${mntpath}/host/usr/local/lib"
 	mount -r -t nullfs /lib "${mntpath}/host/lib"
 	mount -r -t nullfs /usr/lib "${mntpath}/host/usr/lib"
 	mount -r -t nullfs /usr/local/lib "${mntpath}/host/usr/local/lib"
+	;;
+start)
+	# Create a hints file with the host libraries.
+	chroot "${MASTERMNT}" /sbin/ldconfig -f /var/run/ld-elf-host.so.hints /host/lib /host/usr/lib /host/usr/local/lib
 	;;
 *)
 	;;
