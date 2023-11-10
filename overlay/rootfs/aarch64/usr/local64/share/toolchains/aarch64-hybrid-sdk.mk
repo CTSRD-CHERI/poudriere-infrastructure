12cabfb85f4a155b9c1c54f8cd54e4a3357375b4
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-11-10 16:21:00 +0000

    Use toolchain from Poudriere with a hybrid sysroot

--- overlay/rootfs/aarch64/usr/local64/share/toolchains/aarch64-hybrid-sdk.mk
+++ overlay/rootfs/aarch64/usr/local64/share/toolchains/aarch64-hybrid-sdk.mk
@@ -1,10 +1,15 @@
-CC=			/zdata/cheri/output/jail-morello-hybrid/clang
-XCC=			/zdata/cheri/output/jail-morello-hybrid/clang
-CPP=			/zdata/cheri/output/jail-morello-hybrid/clang-cpp
-XCPP=			/zdata/cheri/output/jail-morello-hybrid/clang-cpp
-CXX=			/zdata/cheri/output/jail-morello-hybrid/clang++
-XCXX=			/zdata/cheri/output/jail-morello-hybrid/clang++
-CROSS_BINUTILS_PREFIX=	/zdata/cheri/output/jail-morello-hybrid/
+CC=			/toolchain/bin/clang
+XCC=			/toolchain/bin/clang
+CPP=			/toolchain/bin/clang-cpp
+XCPP=			/toolchain/bin/clang-cpp
+CXX=			/toolchain/bin/clang++
+XCXX=			/toolchain/bin/clang++
+CROSS_BINUTILS_PREFIX=	/toolchain/bin/
 X_COMPILER_TYPE=	clang
 
 CROSS_HOST=		aarch64-unknown-freebsd14.0
+
+# /toolchain/bin/wrapper requires MACHINE_ARCH_HYBRID to be defined to configure
+# the toolchain to cross-compile for the hybrid ABI.
+MACHINE_ARCH_HYBRID=	1
+.export MACHINE_ARCH_HYBRID
--- overlay/usr/local/etc/poudriere.d/hooks/jail.sh
+++ overlay/usr/local/etc/poudriere.d/hooks/jail.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SYSROOT_MORELLO_HYBRID="/zdata/cheri/output/morello-sdk/sysroot-morello-hybrid"
+
 event="${1}"
 
 case "${event}" in
@@ -11,6 +13,17 @@ mount)
 	mount -r -t nullfs /lib "${mntpath}/host/lib"
 	mount -r -t nullfs /usr/lib "${mntpath}/host/usr/lib"
 	mount -r -t nullfs /usr/local/lib "${mntpath}/host/usr/local/lib"
+
+	if [ -d "${SYSROOT_MORELLO_HYBRID}" ]; then
+		# Mount a sysroot to cross-compile hybrid ABI ports that
+		# actually use capabilities.
+		#
+		# The sysroot doesn't have to exist when building CheriABI or
+		# benchmark ABI packages, in which case simply do nothing.
+		mkdir -p "${mntpath}/toolchain/sysroot-morello-hybrid"
+		mount -r -t nullfs "${SYSROOT_MORELLO_HYBRID}" \
+		    "${mntpath}/toolchain/sysroot-morello-hybrid"
+	fi
 	;;
 start)
 	# Create a hints file with the host libraries.
